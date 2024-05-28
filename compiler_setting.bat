eeeda05c43e2b63977c0333cb375ca7259db3cec
Michael Ou <ougengxin@gmail.com>
2024-05-28 11:28:31 -0400

    update to use ifort new executable name

--- compiler_setting.bat
+++ compiler_setting.bat
@@ -1,5 +1,5 @@
 @echo off
-set ff=ifort -traceback -heap-arrays -O2 -fpe:0 -fpp -nowarn -nologo -static
+set ff=ifx -traceback -heap-arrays -O2 -fpe:0 -fpp -nowarn -nologo -static
 set cc=cl /O2 /fp:except /nologo /c
 set lk=link -static 
 
